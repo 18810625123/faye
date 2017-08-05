70539a73ba2d4b9c455bb677f5496925dc4e0863
Liu Dong <chuangye201012@163.com>
2017-08-06 01:54:05 +0800

    添加了管理页面

--- app/models/channel.rb
+++ app/models/channel.rb
@@ -1,19 +1,2 @@
 class Channel < ApplicationRecord
-
-  def users
-    if @users
-      @users
-    else
-      @users = []
-    end
-  end
-
-  def add_user user
-    users << user
-  end
-
-  def user_exist?
-    puts users.size
-  end
-
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
 
+  resources :channels
+  get 'welcome/index'
+
   mount HelloApi, at: '/'
 
+  root 'welcome#index'
 end
